30ba55a998671812f020a0123e2c977e49b5a420
aungxinme <developer.aungzinmoe@gmail.com>
2023-12-01 01:29:13 +0630

    error fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,9 +26,15 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
-});
+    wsHost: import.meta.env.VITE_PUSHER_HOST,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
+    scheme: import.meta.env.VITE_PUSHER_SCHEME
+    });
+
+    // wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    // wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    // forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    // enabledTransports: ['ws', 'wss'],
+
